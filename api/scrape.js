ff9658602a4a684af9a1d4bd0d2bd9ea714b473f
mmazzarolo <me@mazzarolomatteo.com>
2022-02-25 19:54:54 +0100

    Don'g fetch the entire HTML

--- api/scrape.js
+++ api/scrape.js
@@ -1,6 +1,6 @@
 const { parse } = require("url");
 const NextCors = require("nextjs-cors").default;
-const got = require("got");
+const fetch = require("node-fetch");
 const metascraper = require("metascraper")([
   require("metascraper-amazon")(),
   require("metascraper-audio")(),
@@ -23,26 +23,24 @@ const metascraper = require("metascraper")([
   require("metascraper-soundcloud")(),
   require("metascraper-video")(),
 ]);
-const url = require('url');
+const url = require("url");
 
-function isValidHttpUrl(string) {
+function isValidHttpUrl(str) {
   let url;
-  
   try {
-    url = new URL(string);
+    url = new URL(str);
   } catch (_) {
-    return false;  
+    return false;
   }
-
   return url.protocol === "http:" || url.protocol === "https:";
 }
 
 export default async function handler(req, res) {
   await NextCors(req, res, {
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
- });
+  });
 
   const targetUrl = parse(req.url, true).query?.url;
 
@@ -54,8 +52,15 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { body: html, url } = await got(targetUrl);
-    const metadata = await metascraper({ html, url });
+    const response = await fetch(targetUrl);
+    let html = ``;
+    for await (const chunk of response.body) {
+      // Stream the response until we find the end of the </head> tag (we don't
+      // need to fetch the entire HTML response).
+      html += chunk.toString();
+      if (html.includes(`</head>`)) break;
+    }
+    const metadata = await metascraper({ html, url: targetUrl });
     res.setHeader("Cache-Control", "s-maxage=3600");
     res.status(200).json(metadata);
   } catch (err) {
